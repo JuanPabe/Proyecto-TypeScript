b2d25ad982e0a0d32aba5bac00549d54dfb3e40b
JuanPabe <bjuampihotmailcom@gmail.com>
2026-04-05 23:48:34 -0300

    app.ts
    Sistema en TypeScript

---
+++ app.ts
@@ -0,0 +1,96 @@
+// Sistema simple de tienda de ropa
+
+enum Categoria {
+  REMERA = "Remera",
+  PANTALON = "Pantalon",
+  CALZADO = "Calzado"
+}
+
+// clase abstracta
+abstract class Producto {
+  protected id: number
+  protected nombre: string
+  protected precio: number
+  protected stock: number
+  protected categoria: Categoria
+
+  constructor(id:number,nombre:string,precio:number,stock:number,categoria:Categoria){
+    this.id=id
+    this.nombre=nombre
+    this.precio=precio
+    this.stock=stock
+    this.categoria=categoria
+  }
+
+  abstract precioFinal():number
+
+  mostrar(){
+    console.log(this.nombre+" | $"+this.precio+" | stock:"+this.stock)
+  }
+}
+
+// herencia
+class Ropa extends Producto{
+  talle:string
+
+  constructor(id:number,nombre:string,precio:number,stock:number,categoria:Categoria,talle:string){
+    super(id,nombre,precio,stock,categoria)
+    this.talle=talle
+  }
+
+  precioFinal():number{
+    return this.precio
+  }
+}
+
+// otra clase hija
+class Calzado extends Producto{
+  numero:number
+
+  constructor(id:number,nombre:string,precio:number,stock:number,numero:number){
+    super(id,nombre,precio,stock,Categoria.CALZADO)
+    this.numero=numero
+  }
+
+  precioFinal():number{
+    return this.precio
+  }
+}
+
+// clase inventario
+class Tienda{
+
+  productos:Producto[]=[]
+
+  agregar(p:Producto){
+    this.productos.push(p)
+  }
+
+  mostrar(){
+    this.productos.forEach(p=>p.mostrar())
+  }
+
+  ordenarPorPrecio(){
+    this.productos.sort((a,b)=>a.precioFinal()-b.precioFinal())
+  }
+
+}
+
+// programa principal
+
+const tienda=new Tienda()
+
+const p1=new Ropa(1,"Remera blanca",5000,10,Categoria.REMERA,"M")
+const p2=new Ropa(2,"Pantalon jean",12000,5,Categoria.PANTALON,"L")
+const p3=new Calzado(3,"Zapatillas",15000,8,40)
+
+tienda.agregar(p1)
+tienda.agregar(p2)
+tienda.agregar(p3)
+
+console.log("Productos:")
+tienda.mostrar()
+
+console.log("Ordenados por precio:")
+tienda.ordenarPorPrecio()
+tienda.mostrar()
